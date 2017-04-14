b81f046e2c720c49070db509ba85175b6c54d863
Emil Fresk <emil.fresk@gmail.com>
2017-04-14 11:20:29 +0200

    Made the serialization its own file

--- src/test_new_payload.cpp
+++ src/test_new_payload.cpp
@@ -1,89 +1,9 @@
 
 #include <iostream>
-#include <array>
-#include <vector>
-#include <type_traits>
-#include <cstring>
-#include <utility>
-#include <functional>
-#include <exception>
 
 #include "datagram_director.hpp"
+#include "serializable_datagram.hpp"
 
-template < typename T >
-struct serializable_payload
-{
-  /* POD forces a default constructor. */
-  static_assert(std::is_pod< T >::value == true, "Datagram need to be POD.");
-
-  /* Trivially copyable guarantees correct operation for std::memcpy. */
-  static_assert(std::is_trivially_copyable< T >::value == true,
-                "Datagram need to be trivially copyable.");
-
-  /**
-   * @brief
-   *
-   * @param[in]
-   * @tparam
-   *
-   * @return
-   */
-  template < std::size_t N >
-  using serialized_array = std::array< uint8_t, N >;
-
-  using make_serialized_array = serialized_array< sizeof(T) >;
-
-
-  T datagram;
-
-  /* Payload creation from data. */
-  constexpr serializable_payload(T &&datagram) noexcept
-      : datagram{std::move(datagram)}
-  {
-  }
-
-  /* Payload creation from serialized data. */
-  template < std::size_t N >
-  constexpr serializable_payload(serialized_array< N > &data) noexcept
-  {
-    static_assert(N == sizeof(T), "Input serialized_array has wrong size.");
-    std::memcpy(&datagram, data.data(), sizeof(T));
-  }
-
-  /* Payload creation from serialized (vector) data. */
-  constexpr serializable_payload(const std::vector< uint8_t > &data)
-  {
-    if (data.size() == sizeof(T))
-      std::memcpy(&datagram, data.data(), sizeof(T));
-    else
-      throw std::invalid_argument("Vector size is not the same as datagram.");
-  }
-
-  constexpr auto serialize() const noexcept
-  {
-    make_serialized_array buffer;
-    std::memcpy(buffer.data(), &datagram, sizeof(T));
-    return buffer;
-  }
-
-  constexpr void serialize(std::vector< uint8_t > &buffer) const noexcept
-  {
-    auto serial_data = serialize();
-
-    buffer = std::vector< uint8_t >(serial_data.data(),
-                                    serial_data.data() + serial_data.size());
-  }
-
-  constexpr auto size() const noexcept
-  {
-    return sizeof(T);
-  }
-
-  constexpr auto get_datagram() const noexcept
-  {
-    return datagram;
-  }
-};
 
 //
 // -----------------------------------------------------------------------
@@ -135,8 +55,8 @@ int main()
 {
   using namespace std;
 
-  using d1 = serializable_payload< data1 >;
-  using d2 = serializable_payload< data2 >;
+  using d1 = serializable_datagram< data1 >;
+  using d2 = serializable_datagram< data2 >;
 
   datagram_director< data1, data2 > cb;
 
@@ -167,6 +87,9 @@ int main()
 
   cb.execute_callback(datagram1);
   cb.execute_callback(datagram2);
+
+  cout << "Test size d1: " << test3.size() << "\n";
+  cout << "Test size d2: " << d2::size() << "\n";
 
   return 0;
 }
