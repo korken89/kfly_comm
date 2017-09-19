ee21e36e266b03603f60ad924d2a25ffdfc6216b
Emil Fresk <emil.fresk@gmail.com>
2017-09-19 13:34:39 +0200

    Fixed file name

--- src/kfly_comm.cpp
+++ src/kfly_comm.cpp
@@ -3,7 +3,7 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#include "kfly_comm/kfly_comm.h"
+#include "kfly_comm/kfly_comm.hpp"
 
 namespace kfly_comm
 {
